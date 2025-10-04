a3b6a1092e497149049b26be0bd72265d61fe70a
Abhishek kumar arya <abhishek.kr.arya004@gmail.com>
2025-10-04 08:52:00 +0530

    this is first commit

---
+++ SampleProject/src/main/java/CROMAPROJECT/SampleProject/App.java
@@ -0,0 +1,10 @@
+package CROMAPROJECT.SampleProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ SampleProject/src/test/java/CROMAPROJECT/SampleProject/Seleniumdemo.java
@@ -0,0 +1,23 @@
+package CROMAPROJECT.SampleProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Seleniumdemo {
+	
+	
+	
+	
+    @Test
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+		driver.get("https://en.wikipedia.org/wiki/Google_Meet");
+
+	}
+
+}
